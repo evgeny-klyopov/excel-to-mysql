3c69d3279952f145bd3239af20c9ff97fe0abbdc
Evgeny.Klyopov <klepov.e@mamsycorp.ru>
2017-08-10 19:48:55 +0300

    first 2

---
+++ config/db.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'dsn' => 'mysql:host=localhost;dbname=import_excel',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8'
+];
---
+++ src/Import.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: klepov.e
+ * Date: 10.08.2017
+ * Time: 19:48
+ */
